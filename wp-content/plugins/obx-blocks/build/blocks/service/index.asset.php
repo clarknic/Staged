fd624f672eae269a4f4df3c6e35b0907647633fe
Yevhen Nikulin <yevhen.nikulin@kitrum.com>
2025-04-17 10:16:57 +0300

    Adjust service block for mobile

--- wp-content/plugins/obx-blocks/build/blocks/service/index.asset.php
+++ wp-content/plugins/obx-blocks/build/blocks/service/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '283cf7ef70b6aeefde7e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '977a41bc6da5e11ae03a');
